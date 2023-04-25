3354e44ab999c6caa1ec96b8c93eb1c88f375198
Andreas Maucher <andreas-maucher@gmx.de>
2023-04-25 09:37:23 +0200

    build MLX42

--- fractol.h
+++ fractol.h
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRACTOl_H
+# define FRACTOL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <MLX42/MLX42.h>
+
+#define WIDTH 512
+#define HEIGHT 512
+
+static mlx_image_t* image; //!probably needs to be in the file
+
+#endif
